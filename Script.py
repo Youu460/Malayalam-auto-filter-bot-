218665a21504b53fc803ebc678ead288a5b01979
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-11 12:22:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -165,7 +165,9 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION ="""<b>📂 Fɪʟᴇ ɴᴀᴍᴇ : <code>{file_name}</code></b>
+    CAPTION ="""<b>𝐻𝑒𝑙𝑙𝑜 👋 {}</b>😍
+
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ : <code>{file_name}</code></b>
 
 <b> ⚡Thanks For Using RAMANAN🤖♥️</b>
 
